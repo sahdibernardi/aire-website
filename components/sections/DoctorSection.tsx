e5980f3eaf9d348f41f0b90c926624de4a30b60b
Sahra Di Bernardi <sah.dibernardi@gmail.com>
2025-09-10 04:56:09 -0300

    fix: DR Section

--- components/sections/DoctorSection.tsx
+++ components/sections/DoctorSection.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function DoctorSection() {
   return (
-    <section className="bg-aire-light-blue relative overflow-hidden">
+    <section className="bg-aire-light-blue relative overflow-hidden flex-col justify-center items-center">
       {/* Grafismo de fundo */}
       <div className="absolute w-full h-full">
         <Image
@@ -14,66 +14,65 @@ export default function DoctorSection() {
         />
       </div>
       
-      <div className="py-20 px-6 container mx-auto grid lg:grid-cols-2 gap-12 items-center relative z-10">
-        {/* Lado esquerdo - Conteúdo de texto */}
-        <div className="space-y-8">
-          <h2 className="text-3xl lg:text-4xl font-bold text-aire-purple leading-tight">
-            Trajetória e visão que inspiram a AIRE
-          </h2>
-          <h3 className="text-xl font-semibold text-aire-purple">Dra. Camila Adam, Msc, PhD</h3>
+      <div className="py-20 flex flex-col justify-center items-center relative z-10 w-full text-aire-purple">
+        <div className="flex justify-center items-start relative z-10 w-full max-w-6xl px-6">
+          {/* Lado esquerdo - Conteúdo de texto */}
+          <div className="space-y-8 w-1/2 pr-8">
+            <h2 className="text-3xl lg:text-4xl font-bold leading-tight">
+              Trajetória e visão que inspiram a AIRE
+            </h2>
+            <h3 className="text-xl font-semibold">Dra. Camila Adam, Msc, PhD</h3>
 
-          <div className="space-y-6 text-aire-slate">
-            <p className="leading-relaxed">
-              Minha trajetória acadêmica tem início na Universidade Federal de Santa Catarina (UFSC), onde me formei
-              fisioterapeuta e conquistei os títulos de mestra e doutora - conquistas que marcaram profundamente minha
-              caminhada e meu compromisso com a ciência e a excelência na saúde!
-            </p>
-
-            <p className="leading-relaxed">
-              A paixão e dedicação à ciência me proporcionaram oportunidades incríveis, como a de realizar um período
-              do meu doutorado na University College London (UCL), em Londres, uma das universidades mais renomadas do
-              mundo. Essa vivência foi transformadora e permitiu uma imersão nas práticas mais avançadas em saúde
-              cardiopulmonar.
-            </p>
-
-            <p className="leading-relaxed">
-              Nos últimos anos, já no Brasil, realizei milhares de atendimentos em fisioterapia cardiopulmonar,
-              acompanhando adultos e crianças em diferentes fases de suas jornadas de reabilitação. Com mais de 10
-              anos de experiência em saúde e fisioterapia cardiopulmonar, minha atuação clínica se estende por
-              contextos hospitalares, ambulatoriais e domiciliares com vivências práticas que fortaleceram minha
-              expertise técnica.
-            </p>
-
-            <p className="leading-relaxed">
-              Atualmente, sou idealizadora e responsável técnica da AIRE, um serviço que nasceu da vontade de
-              proporcionar atendimentos especializados em fisioterapia cardiovascular, pulmonar e oncológica com
-              excelência. A AIRE foi concebida com base em toda minha formação acadêmica e experiência prática ao
-              longo dos anos e tem o compromisso de transformar recomeços com técnica, ciência e humanização.
-            </p>
-
-            <div className="mt-8 space-y-2 text-sm">
-              <p className="font-semibold">Dra. Camila Thaís Adam, PhD</p>
-              <p>Fisioterapeuta | CREFITO 325911-F</p>
-              <p>
-                Doutora em Ciências Médicas pela Universidade Federal de Santa Catarina (UFSC) e University College
-                London (UCL)
+            <div className="space-y-6">
+              <p className="leading-relaxed">
+              Minha trajetória acadêmica tem início na Universidade Federal de Santa Catarina (UFSC), onde me formei fisioterapeuta e conquistei os títulos de mestra e doutora - conquistas que marcaram profundamente minha caminhada e meu compromisso com a ciência e a excelência na saúde!
               </p>
-              <p>Idealizadora e responsável técnica pela AIRE Cardiopulmonar</p>
+
+              <p className="leading-relaxed">
+              A paixão e dedicação à ciência me proporcionaram oportunidades incríveis, como a de realizar um período do meu doutorado na University College London (UCL), em Londres, uma das universidades mais renomadas do mundo. Essa vivência foi transformadora e permitiu uma imersão nas práticas mais avançadas em saúde cardiopulmonar.
+              </p>
+            </div>
+          </div>
+
+          {/* Lado direito */}
+          <div className="relative flex justify-center items-center w-1/2">
+            <div className="relative">          
+              <div className="relative z-10 w-80 h-120">
+                <Image
+                  src="/camila.jpg"
+                  alt="Dra. Camila Adam"
+                  className="w-full h-full object-cover rounded-full"
+                  fill
+                />
+              </div>
             </div>
           </div>
         </div>
+        
+        <div className="mt-10 flex flex-col justify-center items-start relative z-10 w-full max-w-6xl px-6">
+          <p className="leading-relaxed">
+            Nos últimos anos, já no Brasil, realizei milhares de atendimentos em fisioterapia cardiopulmonar,
+            acompanhando adultos e crianças em diferentes fases de suas jornadas de reabilitação. Com mais de 10
+            anos de experiência em saúde e fisioterapia cardiopulmonar, minha atuação clínica se estende por
+            contextos hospitalares, ambulatoriais e domiciliares com vivências práticas que fortaleceram minha
+            expertise técnica.
+          </p>
 
-        {/* Lado direito - Foto com grafismo rosa vertical */}
-        <div className="relative flex justify-center">
-          <div className="relative">          
-            {/* Foto da Dra. Camila */}
-            <div className="relative z-10 w-80 h-80">
-              <img
-                src="/camila.jpg"
-                alt="Dra. Camila Adam"
-                className="w-full h-full object-cover rounded-full"
-              />
-            </div>
+          <p className="leading-relaxed mt-6">
+            Atualmente, sou idealizadora e responsável técnica da AIRE, um serviço que nasceu da vontade de
+            proporcionar atendimentos especializados em fisioterapia cardiovascular, pulmonar e oncológica com
+            excelência. A AIRE foi concebida com base em toda minha formação acadêmica e experiência prática ao
+            longo dos anos e tem o compromisso de transformar recomeços com técnica, ciência e humanização.
+          </p>
+
+          <div className="mt-8 text-sm font-light">
+            <p>Dra. Camila Thaís Adam, PhD</p>
+            <p>Fisioterapeuta | CREFITO 325911-F</p>
+            <p>
+              Doutora em Ciências Médicas pela Universidade Federal de Santa Catarina (UFSC) e University College
+              London (UCL)
+            </p>
+            <p>Idealizadora e responsável técnica pela AIRE Cardiopulmonar</p>
           </div>
         </div>
       </div>
